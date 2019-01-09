6960aa168a8688c860866fa7d5bd0d8bb10153da
namhun21 <namhun21@naver.com>
2019-01-09 09:46:59 +0900

    [김남훈] image_predict 로그기록 출력되도록 수정

--- image_predict.py
+++ image_predict.py
@@ -5,6 +5,24 @@ import pickle
 import time
 import numpy as np
 import datetime
+import cx_Oracle
+import pandas as pd
+import csv
+
+connstr = "student/student"
+conn = cx_Oracle.connect(connstr)
+curs = conn.cursor()
+result = curs.execute("select * from test")
+result = curs.fetchall()
+
+name_result = [] #db에 있는 학생의 이름 가져오기위해
+check_time = [] #들어온 시간 출력
+check_name = [] #출석이 된 학생의 이름 받아오기위해
+not_in_name = []
+not_in_time=[]
+
+for i in range(len(result)):
+    name_result.append(result[i][0])
 
 now = datetime.datetime.now()
 #print(now)
@@ -33,7 +51,7 @@ process_this_frame = True
 
 # Grab a single frame of video
 
-small_frame = cv2.imread("16.jpg")
+small_frame = cv2.imread("44.jpg")
 
 # Resize frame of video to 1/4 size for faster face recognition processing
 #small_frame = cv2.resize(frame, (0, 0), fx=2.0,  fy=2.0)
@@ -87,27 +105,21 @@ for name, (top, right, bottom, left) in predictions:
     cv2.rectangle(small_frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
     # Draw a label with a name below the face
-    cv2.rectangle(small_frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+    cv2.rectangle(small_frame, (left, bottom - 15), (right, bottom), (0, 0, 255), cv2.FILLED)
     font = cv2.FONT_HERSHEY_DUPLEX
     cv2.putText(small_frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
+    check_time.append(datetime.datetime.now())
+    check_name.append(name)
     cv2.putText(small_frame ,"People: ",(20,200), font,1.0,(255,255,255),1)
+
+
+
 for i in range(len(predictions)):
     cv2.putText(small_frame , predictions[i][0],( (i*120)+ 160 ,200), font,1.0,(255,255,255),1)
 
-elapsed = round((time.clock() - start),4)
-endtime = time.clock()
-endtime2 = time.time()
+        #print(check_time[i])
+
 now2 = datetime.datetime.now()
-#print(now2)
-
-
-
-#print(endtime)
-#print(endtime2)
-
-#print(endtime-start)
-#print(endtime2-start2)
 
 cv2.putText(small_frame , "Start:" + str(now),(20,50), font,1.0,(255,255,255),1)
 cv2.putText(small_frame , "End:" + str(now2),(20,100), font,1.0,(255,255,255),1)
@@ -117,7 +129,39 @@ cv2.imshow('Video', small_frame)
 if not os.path.isdir("./folder"):
     os.mkdir("./folder")
 os.chdir("./folder")
-cv2.imwrite('4_%d_result .jpg' %7,small_frame)
+cv2.imwrite('5_%d_result .jpg' %1,small_frame)
+
+tm = datetime.datetime.now()
+dir_time= tm.strftime('%Y-%m-%d')
+
+path = "./"+dir_time
+if os.path.isdir(path):
+    print("파일 있당")
+    os.chdir(path)
+    read_data = pd.read_csv('check.csv') #기존 csv파일 오픈
+    for i in range(len(check_name)):
+        list_csv=[]
+        for j in range(len(read_data['Name'])):
+            list_csv.append(read_data['Name'][j])
+        if not (check_name[i] in list_csv):
+            not_in_name.append(check_name[i])
+            not_in_time.append(check_time[i])
+    data = {'Name': not_in_name, 'Time': not_in_time}
+    dataframe = pd.DataFrame(data)
+    dataframe.to_csv("check.csv", mode='a',header=False, index=False)
+
+else:
+    print("파일 없음")
+    os.mkdir(path)
+    os.chdir(path)
+    f = open('check.csv', 'w', encoding='utf-8', newline='')
+    wr = csv.writer(f, delimiter='\t')
+    data = {'Name': check_name, 'Time': check_time}
+    dataframe = pd.DataFrame(data)
+    dataframe.to_csv("check.csv", header=True, index=False)
+    f.close()
+
+
 # Hit 'q' on the keyboard to quit!
 #if cv2.waitKey(1) & 0xFF == ord('q'):
  #   break
